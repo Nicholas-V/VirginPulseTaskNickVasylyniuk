6657d533e62aea5c97fe17a5965f5bacc75af2d6
Nicholas-V <MykolaVasylyniuk@gmail.com>
2021-09-29 09:30:57 -0400

    Final commit

--- src/test/java/virgin_pulse/runners/FailedTestRunner.java
+++ src/test/java/virgin_pulse/runners/FailedTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "@target/rerun.txt",
-        glue = "com/cybertek/step_definitions"
+        glue = ""
 )
 public class FailedTestRunner {
 }
